827e99bde883d52ac49608faf247fe5854e97c0d
LuisFabianJorgeCedillo <fabianciittoo@gmail.com>
2023-05-29 17:19:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/consultoriodental/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.consultoriodental
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btniniciarseccion= findViewById<Button>(R.id.btniniciarseccion) as Button
+        val btnregistrarse= findViewById<Button>(R.id.btnregistrarse) as Button
+
+
+        btniniciarseccion.setOnClickListener() {
+            val intent = Intent(this@MainActivity, MainActivity2_inicioseccionpant::class.java)
+            startActivity(intent)
+        }
+
+        btnregistrarse.setOnClickListener() {
+            val intent = Intent(this@MainActivity, MainActivity2_registrarsepant::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/consultoriodental/MainActivity2_registrarsepant.kt
@@ -0,0 +1,16 @@
+package com.example.consultoriodental
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2_registrarsepant : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity2_registrarsepant)
+
+
+    }
+
+
+
+}
